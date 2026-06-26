dccfb139177b14f3f74b2e35f6384601c9e792fb
reza66-90 <mhrabireza66@gmail.com>
2026-06-27 00:38:37 +0330

    Initial website upload
    Initial website upload

---
+++ data/posts.js
@@ -0,0 +1,37 @@
+const posts = [
+  {
+    title: "ایمپلنت دندان چیست؟",
+    url: "articles/implant-guide.html",
+    image: "images/articles/implant.jpg",
+    desc: "آشنایی کامل با ایمپلنت دندان و مراحل انجام آن",
+    category: "ایمپلنت",
+    date: "2026-06-15"
+  },
+
+  {
+    title: "بلیچینگ دندان",
+    url: "articles/bleaching.html",
+    image: "images/articles/bleaching.jpg",
+    desc: "آشنایی با بلیچینگ دندان، مزایا و مراقبت‌های بعد از آن",
+    category: "بلیچینگ",
+    date: "2026-06-16"
+  },
+
+  {
+    title: "لمینت دندان چیست؟",
+    url: "articles/laminate.html",
+    image: "images/articles/laminate.jpg",
+    desc: "مزایا و معایب لمینت دندان",
+    category: "لمینت",
+    date: "2026-06-14"
+  },
+
+  {
+    title: "کامپوزیت دندان چیست؟",
+    url: "articles/composite.html",
+    image: "images/articles/composite.jpg",
+    desc: "مزایا و معایب کامپوزیت",
+    category: "کامپوزیت",
+    date: "2026-06-13"
+  }
+];
