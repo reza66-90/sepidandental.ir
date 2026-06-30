752f2d15cbb0ed7203d384989b4280a6e1afaff5
reza66-90 <mhrabireza66@gmail.com>
2026-06-30 17:49:31 +0330

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,20 +4,22 @@ import {
   collection,
   addDoc,
   getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
   query,
   orderBy,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_REAL_API_KEY_HERE",
+  apiKey: "AIzaSyBh2KCVVifmT5Gzz9NE5A7OnnyS5c2RTsA",
   authDomain: "sepidan-d192d.firebaseapp.com",
   projectId: "sepidan-d192d",
   storageBucket: "sepidan-d192d.firebasestorage.app",
   messagingSenderId: "688114065376",
   appId: "1:688114065376:web:fc4b4b5a120f0cc8a19585"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
@@ -25,6 +27,9 @@ export {
   collection,
   addDoc,
   getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
   query,
   orderBy,
   serverTimestamp
